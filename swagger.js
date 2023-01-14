fafe575694f20d65dda5a9f7df78d769872bc85c
swordgeo <swordgeo1094@yahoo.com>
2023-01-14 13:06:27 -0500

    Changing host rout in swagger-output.js

--- swagger.js
+++ swagger.js
@@ -5,10 +5,11 @@ const doc = {
     title: 'My API',
     description: 'Welcome to my fumbling mess',
   },
-  //Imagine I'll have to keep changing this to the current render link
+  //Imagine I'll have to keep changing this to the current render link\
+  //Also change it in line 8 on swagger-output.js
   host: 'https://cse-341-l4.onrender.com/',
   //host: 'localhost:3000',
-  
+
   //this is originally  http but maybe I'll have to change it to https?
   schemes: ['http'],
 };
